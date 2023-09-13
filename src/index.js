930d0ebf946a3eed25c40bf1cf93ad3702036b94
Marc Nuri <marc@marcnuri.com>
2023-09-13 15:46:38 +0200

    spike: cluster starts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const core = require('@actions/core');
-const logExecSync = require('./exec').logExecSync;
+const {logExecSync} = require('./exec');
+
+const crioVersion = 'v1.22.5'; // latter versions won't work
+const microShiftVersion = '4.8.0-0.microshift-2022-04-20-182108'; // latest binary available on GitHub
+const kubeConfigPath = '/var/lib/microshift/resources/kubeadmin/kubeconfig';
 
 const run = async () => {
   core.info('Updating Environment configuration to support MicroShift');
@@ -11,15 +15,20 @@ const run = async () => {
       ' conntrack' + // needed by cri-o / containers
       ' jq' // needed by cri-o install script
   );
+
   logExecSync(
-    'curl https://raw.githubusercontent.com/cri-o/cri-o/v1.28.1/scripts/get | sudo bash'
+    `curl https://raw.githubusercontent.com/cri-o/cri-o/${crioVersion}/scripts/get | sudo bash`
   );
   logExecSync('sudo systemctl enable crio --now');
+  // Show crictl debug info
+  logExecSync('sudo crictl version');
+
   logExecSync(
-    'curl -LO https://github.com/openshift/microshift/releases/download/4.8.0-0.microshift-2022-04-20-182108/microshift-linux-amd64'
+    `curl -LO https://github.com/openshift/microshift/releases/download/${microShiftVersion}/microshift-linux-amd64`
   );
   logExecSync('sudo chmod a+x microshift-linux-amd64');
   logExecSync('sudo mv microshift-linux-amd64 /usr/local/bin/microshift');
+
   logExecSync(`sudo sh -c 'cat > /usr/lib/systemd/system/microshift.service' << EOF
     [Unit]
     Description=MicroShift
@@ -35,41 +44,26 @@ const run = async () => {
     WantedBy=multi-user.target
   EOF`);
   logExecSync('sudo systemctl enable microshift.service --now');
+  // TODO: see if service can log to a file. Print log contents in case of failure
 
   core.info('Waiting for MicroShift service to start');
   logExecSync('systemctl --no-pager status microshift.service');
-  // Should report (TODO: add check):
-  // ● microshift.service - MicroShift
-  //   Loaded: loaded (/lib/systemd/system/microshift.service; enabled; vendor preset: enabled)
-  //   Active: active (running) since Wed 2023-09-13 11:10:01 UTC; 21ms ago
-  //   Main PID: 2900 (microshift)
-  //   Tasks: 6 (limit: 8300)
-  //   Memory: 2.9M
-  //   CPU: 7ms
-  //   CGroup: /system.slice/microshift.service
-  //              └─2900 /usr/local/bin/microshift run
 
-  const kubeConfigPath = '/var/lib/microshift/resources/kubeadmin/kubeconfig';
   // Wait for MicroShift .kube/config file
   logExecSync(
     `sudo bash -c 'SECONDS=0; until [ -s "${kubeConfigPath}" ] || (( SECONDS >= 30 )); do sleep 1; done'`
   );
   logExecSync(`mkdir -p $HOME/.kube`);
-  logExecSync(`sudo cp -f ${kubeConfigPath} $HOME/.kube/config`);
-  logExecSync(`sudo chmod a+rw /home/runner/.kube/config`);
+  logExecSync(`sudo cat ${kubeConfigPath} > ~/.kube/config`);
 
   core.info('Waiting for MicroShift to be ready');
   // Wait for the Pods to be listed
-
   logExecSync(
-    "bash -c 'SECONDS=0; until kubectl get pods -A | grep kube-system || (( SECONDS >= 720 )); do sleep 1; done'"
+    "bash -c 'SECONDS=0; until kubectl get pods -A | grep kube-system || (( SECONDS >= 90 )); do sleep 1; done'"
   );
+  logExecSync('kubectl wait --for=condition=ready --timeout=30s nodes --all');
   logExecSync(`kubectl get nodes`);
-  logExecSync(`kubectl get pods -A`);
-  logExecSync(
-    'kubectl wait --for=condition=ready --timeout=240s pods --all -n kube-system'
-  );
-  logExecSync(`kubectl get pods -A`);
+  logExecSync('kubectl wait --for=condition=ready --timeout=120s pods --all -A');
 };
 
 run()
